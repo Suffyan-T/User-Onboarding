60d4e34ddfa084eeb7afc0c79368f2620a911248
Suffyan Tariq <suffyan.tariq@gmail.com>
2019-11-15 13:03:19 -0800

    added post display in app

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Form from "./components/Form";
 import "./App.css";
 
+
 function App() {
   return (
     <div className="App">
--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,83 +1,114 @@
-import React from "react";
-import { withFormik, Form, Field } from "formik";
-import * as Yup from "yup";
-import axios from "axios";
+import React, {useState, useEffect} from 'react';
+import { withFormik, Form, Field }from 'formik';
+import * as Yup from 'yup';
+import Axios from 'axios';
 
-function LoginForm({ values, errors, touched }) {
-  return (
-    <Form>
-      <div>
-        {touched.name && errors.name && <p className="error">{errors.name}</p>}
-        <Field type="text" name="name" placeholder="Name" />
-      </div>
-      <div>
-        {touched.email && errors.email && (
-          <p className="error">{errors.email}</p>
-        )}
-        <Field type="email" name="email" placeholder="Email" />
-      </div>
-      <div>
-        {touched.password && errors.password && (
-          <p className="error">{errors.password}</p>
-        )}
-        <Field type="password" name="password" placeholder="Password" />
-      </div>
-      <div>
-        <Field component="select" name="role">
-          <option value="backend">Backend Dev</option>
-          <option value="frontend">Frontend Dev</option>
-          <option value="fullstack">Fullstack Dev</option>
-        </Field>
-      </div>
-      <label>
-        <Field
-          className="terms"
-          type="checkbox"
-          name="terms"
-          checked={values.terms}
-        />
-        Terms of Service
-      </label>
-      <button type="submit">Submit</button>
-    </Form>
-  );
+function InputForm({ values, errors, touched, status}){
+    const [users, setUsers]=useState([]);
+    useEffect(()=>{
+        if (status){
+            setUsers([...users, status])
+        }
+
+    }, [status])
+    return(
+        <div>
+            <Form>
+            <div>
+                {touched.name && errors.name && <p>{errors.name}</p>}
+                <label>
+                     Name:
+                    <Field
+                        type=''
+                        name='name'
+                        placeholder='Name'
+                    />
+                </label>
+            </div>
+            
+            
+                
+            <div>
+                {touched.email && errors.email && <p>{errors.email}</p>}
+                <label>
+                    Email:
+                    <Field
+                        type='email'
+                        name='email'
+                        placeholder='Email'
+                    />
+                </label>
+            </div>
+
+           <div>
+               {touched.errors && errors.password && <p>{errors.password}</p>}
+                <label>
+                    Password:
+                    <Field
+                        type='password'
+                        name='password'
+                        placeholder='Password'
+                    />
+                </label>
+           </div>
+
+            <label>
+                Do you accept Terms and Conditions?
+                <Field
+                type='checkbox'
+                name='terms'
+                checked={values.terms}
+                />
+            </label>
+            
+            <button type='submit'>Submit</button>
+            </Form>
+        {users.map(user=>(
+            <ul key={user.id}>
+                <li key={user.name}>Name:{user.name}</li>
+                <li key={user.email}>Email:{user.email}</li>
+                <li key={user.password}>Password:{user.password}</li>
+            </ul>
+        ))}
+        </div>
+        
+    )
 }
 
-const FormikLoginForm = withFormik({
-  mapPropsToValues({ email, password, name, terms, role }) {
-    return {
-      name: name || "",
-      email: email || "",
-      password: password || "",
-      terms: terms || false,
-      role: role || "backend"
-    };
-  },
+const InputFormApp = withFormik({
+    mapPropsToValues({
+        name,
+        email,
+        password,
+        terms,
+    }){
+        return{
+            name: name||'',
+            email: email||'',
+            password:password||'',
+            terms:terms||false, 
+        }
+    },
+    validationSchema:Yup.object().shape({
+        name:Yup.string().required(),
+        email:Yup.string().email().required(),
+        password: Yup.string().min(8).required(),
 
-  validationSchema: Yup.object().shape({
-    name: Yup.string(),
-    email: Yup.string()
-      .email("Email not valid")
-      .required("Email is required"),
-    password: Yup.string()
-      .min(6)
-      .required("Password is required"),
-    terms: Yup.boolean().required()
-  }),
 
-  handleSubmit(values, { resetForm, setStatus }) {
-    console.log(values);
-    axios
-      .post("https://reqres.in/api/users/", values)
-      .then(response => {
-        console.log(response.data);
-        setStatus(response.data);
-        resetForm();
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
-})(LoginForm);
+    }),
 
-export default FormikLoginForm;
+    handleSubmit(values, {setStatus}){
+        console.log(values);
+        Axios
+        .post('https://reqres.in/api/users', values)
+        .then(res=>{
+            setStatus(res.data); 
+        })
+        .catch(err=>{
+            console.log(err.res);
+        })
+    }
+
+})(InputForm)
+
+export default InputFormApp;
